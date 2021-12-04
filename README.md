2e1094195745c932a153796b016bc97509d36ebb
Diego Soria <diegosoriaarg@gmail.com>
2021-12-04 16:05:01 -0300

    Agregando el proyecto

--- README.md
+++ README.md
@@ -4,7 +4,9 @@ This project was generated with [Angular CLI](https://github.com/angular/angular
 
 ## Development server
 
-Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
+Ejecute `ng serve` para un servidor de desarrollo. Vaya a `http:/localhost:4200/`. La aplicación se recargará automáticamente si cambia alguno de los archivos de origen.
+
+Ejecute `npm run server` para iniciar db.json. Vaya a `http://localhost:5001/tasks`.
 
 ## Code scaffolding
 
--- src/app/service/task.service.ts
+++ src/app/service/task.service.ts
@@ -1,14 +1,20 @@
 import { Injectable } from '@angular/core';
-import {HttpClient, HttpHandler } from '@angular/common/http';
+import {HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import {Task} from '../Task';
 import {TASK} from '../mock-task';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json'
+  })
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
-  private apiUrl = 'http://localhost:5001/task'
+  private apiUrl = 'http://localhost:5001/tasks'
 
   constructor(
     private http: HttpClient
@@ -16,5 +22,19 @@ export class TaskService {
 
   getTasks(): Observable<Task[]>{
     return this.http.get<Task[]>(this.apiUrl)
+  }
+
+  deleteTask(task: Task): Observable<Task>{
+    const url = `${this.apiUrl}/${task.id}`
+    return this.http.delete<Task>(url)
+  }
+
+  updateTaskReminder(task: Task): Observable<Task>{
+    const url = `${this.apiUrl}/${task.id}`
+    return this.http.put<Task>(url, task, httpOptions)
+  }
+
+  addTask(task: Task): Observable<Task>{
+    return this.http.post<Task>(this.apiUrl, task, httpOptions)
   }
 }
